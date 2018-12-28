5d7fef0bdf455f15ca08ebb39c7b1d0b7b0c6a9d
Mustafa <muhammed.mustafa.husrevoglu@gazi.edu.tr>
2018-12-28 22:33:09 +0300

    İlk
    İlk deneme

---
+++ AnalizZet/AnalizZet/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AnalizZet
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lineChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dGV = new System.Windows.Forms.DataGridView();
+            this.veriAl = new System.Windows.Forms.Button();
+            this.basaAl = new System.Windows.Forms.Button();
+            this.saniyeLabel = new System.Windows.Forms.Label();
+            this.grafikCiz = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lineChart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lineChart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lineChart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(13, 13);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(285, 547);
+            this.mediaPlayer.TabIndex = 0;
+            // 
+            // lineChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.lineChart.ChartAreas.Add(chartArea1);
+            this.lineChart.Cursor = System.Windows.Forms.Cursors.Default;
+            legend1.Name = "Legend1";
+            this.lineChart.Legends.Add(legend1);
+            this.lineChart.Location = new System.Drawing.Point(304, 12);
+            this.lineChart.Name = "lineChart";
+            this.lineChart.Size = new System.Drawing.Size(475, 250);
+            this.lineChart.TabIndex = 1;
+            this.lineChart.Text = "chart1";
+            this.lineChart.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lineChart_MouseClick);
+            this.lineChart.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lineChart_MouseMove);
+            // 
+            // dGV
+            // 
+            this.dGV.AllowUserToOrderColumns = true;
+            this.dGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV.Location = new System.Drawing.Point(13, 566);
+            this.dGV.Name = "dGV";
+            this.dGV.ReadOnly = true;
+            this.dGV.Size = new System.Drawing.Size(1163, 136);
+            this.dGV.TabIndex = 2;
+            // 
+            // veriAl
+            // 
+            this.veriAl.Location = new System.Drawing.Point(1286, 9);
+            this.veriAl.Name = "veriAl";
+            this.veriAl.Size = new System.Drawing.Size(52, 23);
+            this.veriAl.TabIndex = 3;
+            this.veriAl.Text = "Devam";
+            this.veriAl.UseVisualStyleBackColor = true;
+            this.veriAl.Click += new System.EventHandler(this.veriAl_Click);
+            // 
+            // basaAl
+            // 
+            this.basaAl.Location = new System.Drawing.Point(1286, 38);
+            this.basaAl.Name = "basaAl";
+            this.basaAl.Size = new System.Drawing.Size(52, 23);
+            this.basaAl.TabIndex = 6;
+            this.basaAl.Text = "Durdur";
+            this.basaAl.UseVisualStyleBackColor = true;
+            this.basaAl.Click += new System.EventHandler(this.basaAl_Click);
+            // 
+            // saniyeLabel
+            // 
+            this.saniyeLabel.AutoSize = true;
+            this.saniyeLabel.Location = new System.Drawing.Point(1176, 74);
+            this.saniyeLabel.Name = "saniyeLabel";
+            this.saniyeLabel.Size = new System.Drawing.Size(0, 13);
+            this.saniyeLabel.TabIndex = 7;
+            // 
+            // grafikCiz
+            // 
+            this.grafikCiz.Location = new System.Drawing.Point(1286, 69);
+            this.grafikCiz.Name = "grafikCiz";
+            this.grafikCiz.Size = new System.Drawing.Size(52, 51);
+            this.grafikCiz.TabIndex = 8;
+            this.grafikCiz.Text = "Ayrı Grafik Çiz";
+            this.grafikCiz.UseVisualStyleBackColor = true;
+            this.grafikCiz.Click += new System.EventHandler(this.grafikCiz_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lineChart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.lineChart1.ChartAreas.Add(chartArea2);
+            this.lineChart1.Cursor = System.Windows.Forms.Cursors.Default;
+            legend2.Name = "Legend1";
+            this.lineChart1.Legends.Add(legend2);
+            this.lineChart1.Location = new System.Drawing.Point(304, 294);
+            this.lineChart1.Name = "lineChart1";
+            this.lineChart1.Size = new System.Drawing.Size(475, 250);
+            this.lineChart1.TabIndex = 9;
+            this.lineChart1.Text = "chart1";
+            this.lineChart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lineChart1_MouseClick);
+            // 
+            // lineChart2
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.lineChart2.ChartAreas.Add(chartArea3);
+            this.lineChart2.Cursor = System.Windows.Forms.Cursors.Default;
+            legend3.Name = "Legend1";
+            this.lineChart2.Legends.Add(legend3);
+            this.lineChart2.Location = new System.Drawing.Point(803, 13);
+            this.lineChart2.Name = "lineChart2";
+            this.lineChart2.Size = new System.Drawing.Size(475, 250);
+            this.lineChart2.TabIndex = 10;
+            this.lineChart2.Text = "chart1";
+            this.lineChart2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lineChart2_MouseClick);
+            // 
+            // lineChart3
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.lineChart3.ChartAreas.Add(chartArea4);
+            this.lineChart3.Cursor = System.Windows.Forms.Cursors.Default;
+            legend4.Name = "Legend1";
+            this.lineChart3.Legends.Add(legend4);
+            this.lineChart3.Location = new System.Drawing.Point(803, 294);
+            this.lineChart3.Name = "lineChart3";
+            this.lineChart3.Size = new System.Drawing.Size(475, 250);
+            this.lineChart3.TabIndex = 11;
+            this.lineChart3.Text = "chart1";
+            this.lineChart3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lineChart3_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.lineChart3);
+            this.Controls.Add(this.lineChart2);
+            this.Controls.Add(this.lineChart1);
+            this.Controls.Add(this.grafikCiz);
+            this.Controls.Add(this.saniyeLabel);
+            this.Controls.Add(this.basaAl);
+            this.Controls.Add(this.veriAl);
+            this.Controls.Add(this.dGV);
+            this.Controls.Add(this.lineChart);
+            this.Controls.Add(this.mediaPlayer);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lineChart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+        private System.Windows.Forms.DataVisualization.Charting.Chart lineChart;
+        private System.Windows.Forms.DataGridView dGV;
+        private System.Windows.Forms.Button veriAl;
+        private System.Windows.Forms.Button basaAl;
+        private System.Windows.Forms.Label saniyeLabel;
+        private System.Windows.Forms.Button grafikCiz;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart lineChart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart lineChart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart lineChart3;
+    }
+}
+
---
+++ AnalizZet/AnalizZet/girisEkrani.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AnalizZet
+{
+    partial class girisEkrani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.veriSecBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.veriYoluTxt = new System.Windows.Forms.TextBox();
+            this.videoTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.videoBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // veriSecBtn
+            // 
+            this.veriSecBtn.Location = new System.Drawing.Point(215, 96);
+            this.veriSecBtn.Name = "veriSecBtn";
+            this.veriSecBtn.Size = new System.Drawing.Size(75, 23);
+            this.veriSecBtn.TabIndex = 0;
+            this.veriSecBtn.Text = "Veri Seç";
+            this.veriSecBtn.UseVisualStyleBackColor = true;
+            this.veriSecBtn.Click += new System.EventHandler(this.veriSecBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(48, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sensör Verilerini Seçiniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(48, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seçilen Veri Yolu:";
+            // 
+            // veriYoluTxt
+            // 
+            this.veriYoluTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.veriYoluTxt.Location = new System.Drawing.Point(190, 125);
+            this.veriYoluTxt.Name = "veriYoluTxt";
+            this.veriYoluTxt.Size = new System.Drawing.Size(100, 23);
+            this.veriYoluTxt.TabIndex = 3;
+            // 
+            // videoTxt
+            // 
+            this.videoTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.videoTxt.Location = new System.Drawing.Point(190, 260);
+            this.videoTxt.Name = "videoTxt";
+            this.videoTxt.Size = new System.Drawing.Size(100, 23);
+            this.videoTxt.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(48, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Seçilen Video Yolu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(48, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Video Kaydı Seçiniz:";
+            // 
+            // videoBtn
+            // 
+            this.videoBtn.Location = new System.Drawing.Point(215, 231);
+            this.videoBtn.Name = "videoBtn";
+            this.videoBtn.Size = new System.Drawing.Size(75, 23);
+            this.videoBtn.TabIndex = 4;
+            this.videoBtn.Text = "Video Seç";
+            this.videoBtn.UseVisualStyleBackColor = true;
+            // 
+            // girisEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 413);
+            this.Controls.Add(this.videoTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.videoBtn);
+            this.Controls.Add(this.veriYoluTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.veriSecBtn);
+            this.Name = "girisEkrani";
+            this.Text = "girisEkrani";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button veriSecBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox veriYoluTxt;
+        private System.Windows.Forms.TextBox videoTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button videoBtn;
+    }
+}
